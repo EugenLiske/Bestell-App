00d5d6e7bb2aa279239af8f8d1fc12172682252d
EugenLiske <eugen.liske@yahoo.de>
2025-05-21 20:22:38 +0200

    dishes db added

---
+++ scripts/dishes_db.js
@@ -0,0 +1,27 @@
+let dishes = [
+    {
+      "name": "Chicken Tikka Masala",
+      "price": 12.90,
+      "description": "Zart gegrillte Hähnchenstücke in einer cremigen, leicht pikanten Tomaten-Curry-Sauce - ein Klassiker der indischen Küche."
+    },
+    {
+      "name": "Palak Paneer",
+      "price": 11.50,
+      "description": "Hausgemachter indischer Frischkäse in einem feinen Spinat-Curry, gewürzt mit traditionellen indischen Gewürzen - vegetarisch und herzhaft."
+    },
+    {
+      "name": "Lamb Rogan Josh",
+      "price": 13.90,
+      "description": "Zartes Lammfleisch langsam gegart in einer aromatischen Sauce aus Tomaten, Joghurt und Gewürzen aus Kaschmir."
+    },
+    {
+      "name": "Chana Masala",
+      "price": 10.50,
+      "description": "Kichererbsen in einer würzigen Tomaten-Zwiebel-Sauce, abgerundet mit frischem Koriander - vegan, proteinreich und sättigend."
+    },
+    {
+      "name": "Aloo Gobi",
+      "price": 10.90,
+      "description": "Ein klassisches nordindisches Curry aus Kartoffeln und Blumenkohl, geschmort mit Tomaten, Ingwer und aromatischen Gewürzen - 100 % vegan und voller Geschmack."
+    },
+];
