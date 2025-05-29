6cada89c17ec9f2f4d831ec5867dfa87946270c1
EugenLiske <eugen.liske@yahoo.de>
2025-05-29 18:11:03 +0200

    order dishes function added

--- script.js
+++ script.js
@@ -30,6 +30,9 @@ function addDishtoBasket(dishIndex){
     let currentPriceOfTheOrderedDishesRef = document.getElementById(`priceOfDish${dishIndex}`);
 
     document.getElementById('empty_basket_information').classList.add('d_none');
+    document.getElementById('basket_bottom_separator').classList.remove('d_none');
+    document.getElementById('total_price_area').classList.remove('d_none');
+    
     
     if (dishes[dishIndex].amount == 0) {
         dishes[dishIndex].amount++;
@@ -39,6 +42,7 @@ function addDishtoBasket(dishIndex){
         currentAmountOfTheOrderedDishesRef.innerHTML = dishes[dishIndex].amount;
         currentPriceOfTheOrderedDishesRef.innerHTML = (dishes[dishIndex].amount * dishes[dishIndex].price).toFixed(2) + ' €';
     }
+    calculateTotalPriceInTheBasket();
 }
 
 function renderSingleBasketDish(dishIndex){
@@ -47,13 +51,13 @@ function renderSingleBasketDish(dishIndex){
                 <h4>${dishes[dishIndex].name}</h4>
                 <div class="calculation_bar">
                     <div class="add_subtract_dish_bar">
-                        <img onclick="subtractDish(${dishIndex})" class="basket_icons" src="/assets/icons/logo/orange_minus_basket.png" alt="">
+                        <img onclick="subtractDish(${dishIndex})" class="basket_icons_plus_minus" src="/assets/icons/logo/orange_minus_basket.png" alt="">
                         <p id="amountOfDish${dishIndex}">${dishes[dishIndex].amount}</p>
-                        <img onclick="addDish(${dishIndex})" class="basket_icons" src="/assets/icons/logo/orange_plus_basket.png" alt="">
+                        <img onclick="addDish(${dishIndex})" class="basket_icons_plus_minus" src="/assets/icons/logo/orange_plus_basket.png" alt="">
                     </div>
                     <div class="result_and_remove_dish_bar">
                         <p id="priceOfDish${dishIndex}">${(dishes[dishIndex].price).toFixed(2)} €</p>
-                        <img class="basket_icons" src="/assets/icons/logo/remove_dishes_basket.png" alt="">
+                        <img onclick="removeSingleDishFromBasket(${dishIndex})" class="delete_dish_icon" src="/assets/icons/logo/remove_dishes_basket.png" alt="">
                     </div>
                 </div>
             </div>
@@ -69,21 +73,18 @@ function addDish(dishIndex){
 
     let newPriceAddition = (dishes[dishIndex].amount * dishes[dishIndex].price).toFixed(2);
     currentPriceOfTheOrderedDishesRef.innerHTML = newPriceAddition + ' €';
+    calculateTotalPriceInTheBasket();
 }
 
 function subtractDish(dishIndex){
     let currentAmountOfTheOrderedDishesRef = document.getElementById(`amountOfDish${dishIndex}`);
     let currentPriceOfTheOrderedDishesRef = document.getElementById(`priceOfDish${dishIndex}`);
     let singleDishInsideTheBasketRef = document.getElementById(`single_dish_inside_the_basket${dishIndex}`);
-    let basketContainerRef = document.getElementById('basket');
 
     if (dishes[dishIndex].amount  == 1) {
         dishes[dishIndex].amount--;
         singleDishInsideTheBasketRef.remove();
-
-        if(dishes[0].amount == 0 && dishes[1].amount == 0 && dishes[2].amount == 0 && dishes[3].amount == 0 && dishes[4].amount == 0){
-            document.getElementById('empty_basket_information').classList.remove('d_none');
-        }
+        displayEmptyBasket();
 
     } else {
         let newPriceSubtraction = ((((dishes[dishIndex].amount * dishes[dishIndex].price) / dishes[dishIndex].amount)) * (dishes[dishIndex].amount - 1)).toFixed(2);
@@ -91,5 +92,51 @@ function subtractDish(dishIndex){
 
         dishes[dishIndex].amount--;
         currentAmountOfTheOrderedDishesRef.innerHTML = dishes[dishIndex].amount;
+    }
+    calculateTotalPriceInTheBasket();
+}
+
+function removeSingleDishFromBasket(dishIndex) {
+    dishes[dishIndex].amount = 0;
+    let singleDishInsideTheBasketRef = document.getElementById(`single_dish_inside_the_basket${dishIndex}`);
+    singleDishInsideTheBasketRef.remove();
+    calculateTotalPriceInTheBasket()
+    displayEmptyBasket();
+}
+
+function displayEmptyBasket(){
+    let totalAmountOfOrderedDishes = 0;
+
+    for (let countTotalDishesIndex = 0; countTotalDishesIndex < dishes.length; countTotalDishesIndex++) {
+        totalAmountOfOrderedDishes += dishes[countTotalDishesIndex].amount;
+    }
+
+    if(totalAmountOfOrderedDishes == 0){
+        document.getElementById('empty_basket_information').classList.remove('d_none');
+        document.getElementById('basket_bottom_separator').classList.add('d_none');
+        document.getElementById('total_price_area').classList.add('d_none');
+    }
+}
+
+function calculateTotalPriceInTheBasket(){
+    let totalPriceRef = document.getElementById('total_price');
+    let totalPriceSum = 0;
+
+    for (let totalPriceIndex = 0; totalPriceIndex < dishes.length; totalPriceIndex++) {
+        totalPriceSum += (dishes[totalPriceIndex].amount * dishes[totalPriceIndex].price);
+    }
+    totalPriceRef.innerHTML = (totalPriceSum).toFixed(2) + ' €';
+}
+
+function orderDishes(){
+    document.getElementById('basket_bottom_separator').classList.add('d_none');
+    document.getElementById('total_price_area').classList.add('d_none');
+    // document.getElementById('basket').classList.add('d_none');
+
+    for (let orderIndex = 0; orderIndex < dishes.length; orderIndex++) {
+        dishes[orderIndex].amount = 0;
+        let singleDishInsideTheBasketRef = document.getElementById(`single_dish_inside_the_basket${orderIndex}`);
+        singleDishInsideTheBasketRef.classList.add('d_none');
+
     }
 }
