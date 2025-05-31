7a692eaf7ea531c2d043e01e15914e64858066a3
EugenLiske <eugen.liske@yahoo.de>
2025-05-31 11:54:38 +0200

    responsive menu, content and functionality added

--- script.js
+++ script.js
@@ -29,20 +29,31 @@ function addDishtoBasket(dishIndex){
     let currentAmountOfTheOrderedDishesRef = document.getElementById(`amountOfDish${dishIndex}`);
     let currentPriceOfTheOrderedDishesRef = document.getElementById(`priceOfDish${dishIndex}`);
 
-    
+    let responsiveBasketContainerRef = document.getElementById('responsive_basket_dishes_container');
+    let currentAmountOfTheOrderedDishesResponsiveRef = document.getElementById(`amountOfDishResponsive${dishIndex}`);
+    let currentPriceOfTheOrderedDishesResponsiveRef = document.getElementById(`priceOfDishResponsive${dishIndex}`);
 
     document.getElementById('empty_basket_information').classList.add('d_none');
+
     document.getElementById('basket_bottom_separator').classList.remove('d_none');
     document.getElementById('total_price_area').classList.remove('d_none');
     document.getElementById('after_order_message_container').classList.add('d_none');
+
+    document.getElementById('basket_bottom_separator_responsive').classList.remove('d_none');
+    document.getElementById('total_price_area_responsive').classList.remove('d_none');
+    document.getElementById('after_order_message_container_responsive').classList.add('d_none');
     
     if (dishes[dishIndex].amount == 0) {
         dishes[dishIndex].amount++;
         basketContainerRef.innerHTML += renderSingleBasketDish(dishIndex);
+        responsiveBasketContainerRef.innerHTML += renderSingleBasketDishResponsive(dishIndex);
     } else {
         dishes[dishIndex].amount++;
         currentAmountOfTheOrderedDishesRef.innerHTML = dishes[dishIndex].amount;
         currentPriceOfTheOrderedDishesRef.innerHTML = (dishes[dishIndex].amount * dishes[dishIndex].price).toFixed(2) + ' €';
+
+        currentAmountOfTheOrderedDishesResponsiveRef.innerHTML = dishes[dishIndex].amount;
+        currentPriceOfTheOrderedDishesResponsiveRef.innerHTML = (dishes[dishIndex].amount * dishes[dishIndex].price).toFixed(2) + ' €';
     }
     calculateTotalPriceInTheBasket();
 }
@@ -66,34 +77,65 @@ function renderSingleBasketDish(dishIndex){
     `
 }
 
+function renderSingleBasketDishResponsive(dishIndex){
+    return `
+            <div id="single_dish_inside_the_basket_responsive${dishIndex}" class="single_dish_inside_the_basket">
+                <h4>${dishes[dishIndex].name}</h4>
+                <div class="calculation_bar">
+                    <div class="add_subtract_dish_bar">
+                        <img onclick="subtractDish(${dishIndex})" class="basket_icons_plus_minus" src="/assets/icons/logo/orange_minus_basket.png" alt="">
+                        <p id="amountOfDishResponsive${dishIndex}">${dishes[dishIndex].amount}</p>
+                        <img onclick="addDish(${dishIndex})" class="basket_icons_plus_minus" src="/assets/icons/logo/orange_plus_basket.png" alt="">
+                    </div>
+                    <div class="result_and_remove_dish_bar">
+                        <p id="priceOfDishResponsive${dishIndex}">${(dishes[dishIndex].price).toFixed(2)} €</p>
+                        <img onclick="removeSingleDishFromBasket(${dishIndex})" class="delete_dish_icon" src="/assets/icons/logo/remove_dishes_basket.png" alt="">
+                    </div>
+                </div>
+            </div>
+    `
+}
+
 function addDish(dishIndex){
     let currentAmountOfTheOrderedDishesRef = document.getElementById(`amountOfDish${dishIndex}`);
     let currentPriceOfTheOrderedDishesRef = document.getElementById(`priceOfDish${dishIndex}`);
 
+    let currentAmountOfTheOrderedDishesResponsiveRef = document.getElementById(`amountOfDishResponsive${dishIndex}`);
+    let currentPriceOfTheOrderedDishesResponsiveRef = document.getElementById(`priceOfDishResponsive${dishIndex}`);
+
     dishes[dishIndex].amount++;
     currentAmountOfTheOrderedDishesRef.innerHTML = dishes[dishIndex].amount;
+    currentAmountOfTheOrderedDishesResponsiveRef.innerHTML = dishes[dishIndex].amount;
 
     let newPriceAddition = (dishes[dishIndex].amount * dishes[dishIndex].price).toFixed(2);
     currentPriceOfTheOrderedDishesRef.innerHTML = newPriceAddition + ' €';
+    currentPriceOfTheOrderedDishesResponsiveRef.innerHTML = newPriceAddition + ' €';
     calculateTotalPriceInTheBasket();
 }
 
 function subtractDish(dishIndex){
+    let singleDishInsideTheBasketRef = document.getElementById(`single_dish_inside_the_basket${dishIndex}`);
     let currentAmountOfTheOrderedDishesRef = document.getElementById(`amountOfDish${dishIndex}`);
     let currentPriceOfTheOrderedDishesRef = document.getElementById(`priceOfDish${dishIndex}`);
-    let singleDishInsideTheBasketRef = document.getElementById(`single_dish_inside_the_basket${dishIndex}`);
+
+    let singleDishInsideTheBasketResponsiveRef = document.getElementById(`single_dish_inside_the_basket_responsive${dishIndex}`);
+    let currentAmountOfTheOrderedDishesResponsiveRef = document.getElementById(`amountOfDishResponsive${dishIndex}`);
+    let currentPriceOfTheOrderedDishesResponsiveRef = document.getElementById(`priceOfDishResponsive${dishIndex}`);
 
     if (dishes[dishIndex].amount  == 1) {
         dishes[dishIndex].amount--;
         singleDishInsideTheBasketRef.remove();
+        singleDishInsideTheBasketResponsiveRef.remove();
         displayEmptyBasket();
 
     } else {
         let newPriceSubtraction = ((((dishes[dishIndex].amount * dishes[dishIndex].price) / dishes[dishIndex].amount)) * (dishes[dishIndex].amount - 1)).toFixed(2);
         currentPriceOfTheOrderedDishesRef.innerHTML = newPriceSubtraction + ' €';
+        currentPriceOfTheOrderedDishesResponsiveRef.innerHTML = newPriceSubtraction + ' €';
 
         dishes[dishIndex].amount--;
         currentAmountOfTheOrderedDishesRef.innerHTML = dishes[dishIndex].amount;
+        currentAmountOfTheOrderedDishesResponsiveRef.innerHTML = dishes[dishIndex].amount;
     }
     calculateTotalPriceInTheBasket();
 }
@@ -102,6 +144,10 @@ function removeSingleDishFromBasket(dishIndex) {
     dishes[dishIndex].amount = 0;
     let singleDishInsideTheBasketRef = document.getElementById(`single_dish_inside_the_basket${dishIndex}`);
     singleDishInsideTheBasketRef.remove();
+
+    let singleDishInsideTheBasketResponsiveRef = document.getElementById(`single_dish_inside_the_basket_responsive${dishIndex}`);
+    singleDishInsideTheBasketResponsiveRef.remove();
+
     calculateTotalPriceInTheBasket()
     displayEmptyBasket();
 }
@@ -122,12 +168,14 @@ function displayEmptyBasket(){
 
 function calculateTotalPriceInTheBasket(){
     let totalPriceRef = document.getElementById('total_price');
+    let totalPriceResponsiveRef = document.getElementById('total_price_responsive');
     let totalPriceSum = 0;
 
     for (let totalPriceIndex = 0; totalPriceIndex < dishes.length; totalPriceIndex++) {
         totalPriceSum += (dishes[totalPriceIndex].amount * dishes[totalPriceIndex].price);
     }
     totalPriceRef.innerHTML = (totalPriceSum).toFixed(2) + ' €';
+    totalPriceResponsiveRef.innerHTML = (totalPriceSum).toFixed(2) + ' €';
 }
 
 function orderDishes(){
@@ -135,15 +183,35 @@ function orderDishes(){
     document.getElementById('total_price_area').classList.add('d_none'); // ist die Nutzung von d_none in Ordnung?
     document.getElementById('after_order_message_container').classList.remove('d_none');
 
+    document.getElementById('basket_bottom_separator_responsive').classList.add('d_none');
+    document.getElementById('total_price_area_responsive').classList.add('d_none');
+    document.getElementById('after_order_message_container_responsive').classList.remove('d_none');
+
     for (let orderIndex = 0; orderIndex < dishes.length; orderIndex++) {
         dishes[orderIndex].amount = 0;
         let singleDishInsideTheBasketRef = document.getElementById(`single_dish_inside_the_basket${orderIndex}`);
+        let singleDishInsideTheBasketResponsiveRef = document.getElementById(`single_dish_inside_the_basket_responsive${orderIndex}`);
 
         if(singleDishInsideTheBasketRef != null){
-        singleDishInsideTheBasketRef.remove();
+            singleDishInsideTheBasketRef.remove();
+            singleDishInsideTheBasketResponsiveRef.remove();
         }
     }
 }
+
+// function orderDishesResponsive(){
+//     document.getElementById('basket_bottom_separator_responsive').classList.add('d_none');
+//     document.getElementById('total_price_area_responsive').classList.add('d_none');
+
+//     for (let orderIndex = 0; orderIndex < dishes.length; orderIndex++) {
+//         dishes[orderIndex].amount = 0;
+//         let singleDishInsideTheBasketResponsiveRef = document.getElementById(`single_dish_inside_the_basket_responsive${orderIndex}`);
+
+//         if(singleDishInsideTheBasketResponsiveRef != null){
+//             singleDishInsideTheBasketResponsiveRef.remove();
+//         }
+//     }
+// }
 
 function toggleResponsiveBasket(){
     document.getElementById('responsive_basket').classList.toggle('responsive_basket_closed');
